e80bd99c77b31725634cc0859269a337ff0dc89a
zombiej <smith3816@gmail.com>
2022-01-25 16:53:04 +0800

    docs: mix theme demo

--- docs/examples/theme.tsx
+++ docs/examples/theme.tsx
@@ -2,27 +2,34 @@ import React from 'react';
 import Button from './components/Button';
 import { DesignToken, DesignTokenContext } from './components/theme';
 
+const ButtonList = () => (
+  <div style={{ background: 'rgba(0,0,0,0.1)', padding: 16 }}>
+    <Button>Default</Button>
+    <Button type="primary">Primary</Button>
+    <Button type="ghost">Ghost</Button>
+  </div>
+);
+
 export default function App() {
   const redTheme: Partial<DesignToken> = {
     primaryColor: 'red',
+  };
+  const orangeTheme: Partial<DesignToken> = {
+    primaryColor: 'orange',
   };
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', rowGap: 8 }}>
       <h3>混合主题</h3>
 
-      <div style={{ background: 'rgba(0,0,0,0.1)', padding: 16 }}>
-        <Button>Default</Button>
-        <Button type="primary">Primary</Button>
-        <Button type="ghost">Ghost</Button>
-      </div>
+      <ButtonList />
 
       <DesignTokenContext.Provider value={{ token: redTheme, hashed: true }}>
-        <div style={{ background: 'rgba(0,0,0,0.1)', padding: 16 }}>
-          <Button>Default</Button>
-          <Button type="primary">Primary</Button>
-          <Button type="ghost">Ghost</Button>
-        </div>
+        <ButtonList />
+      </DesignTokenContext.Provider>
+
+      <DesignTokenContext.Provider value={{ token: orangeTheme, hashed: true }}>
+        <ButtonList />
       </DesignTokenContext.Provider>
     </div>
   );
