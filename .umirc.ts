b830d6b7e35548b5798f61155629a1534311c1bd
zombiej <smith3816@gmail.com>
2022-01-26 16:58:41 +0800

    docs: fix umi path

--- .umirc.ts
+++ .umirc.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   outputPath: '.doc',
   exportStatic: {},
-  base: '/footer/',
-  publicPath: '/footer/',
+  base: '/',
+  publicPath: '/',
   hash: true,
   styles: [
     `
