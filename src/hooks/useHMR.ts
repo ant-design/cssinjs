a4aa0be313fc93d3b7d5ce877085d77116b169b1
二货机器人 <smith3816@gmail.com>
2022-09-26 14:56:12 +0800

    fix: Vite not exist module

--- src/hooks/useHMR.ts
+++ src/hooks/useHMR.ts
@@ -12,7 +12,12 @@ export default process.env.NODE_ENV === 'production' ? useProdHMR : useDevHMR;
 
 // Webpack `module.hot.accept` do not support any deps update trigger
 // We have to hack handler to force mark as HRM
-if (process.env.NODE_ENV !== 'production' && module && module.hot) {
+if (
+  process.env.NODE_ENV !== 'production' &&
+  typeof module !== 'undefined' &&
+  module &&
+  module.hot
+) {
   const win = window as any;
   if (typeof win.webpackHotUpdate === 'function') {
     const originWebpackHotUpdate = win.webpackHotUpdate;
