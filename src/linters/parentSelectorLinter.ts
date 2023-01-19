52381db87e2cfc17ef90734a16d1d75fddc41752
MadCcc <1075746765@qq.com>
2023-01-19 15:33:43 +0800

    fix: parent selector lint should recognize comma

--- src/linters/parentSelectorLinter.ts
+++ src/linters/parentSelectorLinter.ts
@@ -3,8 +3,11 @@ import { lintWarning } from './utils';
 
 const linter: Linter = (key, value, info) => {
   const currentSelector = info.parentSelectors[info.parentSelectors.length - 1];
-  if (currentSelector && currentSelector.split('&').length > 2) {
-    lintWarning('Should not use more than one `&` in a selector.', info);
+  if (currentSelector) {
+    const selectors = currentSelector.split(',');
+    if (selectors.some((selector) => selector.split('&').length > 2)) {
+      lintWarning('Should not use more than one `&` in a selector.', info);
+    }
   }
 };
 
