ec7b266961b595c85577ad28d9501854f07c6a1a
wuxh <wxh1220@gmail.com>
2023-02-26 11:55:22 +0800

    chore: update .dumirc

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,9 @@
 // more config: https://d.umijs.org/config
 import { defineConfig } from 'dumi';
 
+const basePath = process.env.GITHUB_ACTIONS ? '/cssinjs/' : '/';
+const publicPath = process.env.GITHUB_ACTIONS ? '/cssinjs/' : '/';
+
 export default defineConfig({
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
   themeConfig: {
@@ -9,4 +12,6 @@ export default defineConfig({
   },
   outputPath: '.doc',
   exportStatic: {},
+  base: basePath,
+  publicPath: publicPath,
 });
