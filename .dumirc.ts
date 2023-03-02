606927b4a06e70b7cfd7310407e5a98aa3030037
wuxh <wxh1220@gmail.com>
2023-03-02 18:07:00 +0800

    chore: update

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'dumi';
 
 const name = 'cssinjs';
 
-const isDeploy = [process.env.PREVIEW, process.env.GITHUB_ACTIONS].includes(
-  'true',
-);
+const isDeployGitHubPage = [
+  process.env.GH_PAGE,
+  process.env.GITHUB_ACTIONS,
+].includes('true');
 
 export default defineConfig({
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
@@ -15,6 +16,6 @@ export default defineConfig({
   },
   outputPath: '.doc',
   exportStatic: {},
-  base: isDeploy ? `/${name}/` : '/',
-  publicPath: isDeploy ? `/${name}/` : '/',
+  base: isDeployGitHubPage ? `/${name}/` : '/',
+  publicPath: isDeployGitHubPage ? `/${name}/` : '/',
 });
