a55e7b748ee33b4568d928135bcbd27c05ee7fdd
wuxh <wxh1220@gmail.com>
2023-03-02 17:35:46 +0800

    chore: update

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,17 +1,20 @@
 // more config: https://d.umijs.org/config
 import { defineConfig } from 'dumi';
 
-const basePath = process.env.GITHUB_ACTIONS ? '/cssinjs/' : '/';
-const publicPath = process.env.GITHUB_ACTIONS ? '/cssinjs/' : '/';
+const name = 'cssinjs';
+
+const isDeploy = [process.env.PREVIEW, process.env.GITHUB_ACTIONS].includes(
+  'true',
+);
 
 export default defineConfig({
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
   themeConfig: {
-    name: 'cssinjs',
+    name,
     logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
   outputPath: '.doc',
   exportStatic: {},
-  base: basePath,
-  publicPath: publicPath,
+  base: isDeploy ? `/${name}/` : '/',
+  publicPath: isDeploy ? `/${name}/` : '/',
 });
