e4567c473912636db66edff644bf3ebc6f2251c6
二货机器人 <smith3816@gmail.com>
2023-03-23 14:00:59 +0800

    docs: add demo of animation

--- docs/examples/shadow.tsx
+++ docs/examples/shadow.tsx
@@ -1,12 +1,18 @@
+import { createCache, StyleProvider } from '@ant-design/cssinjs';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { StyleProvider } from '@ant-design/cssinjs';
 import Button from './components/Button';
+import Spin from './components/Spin';
 
 export default function App() {
+  const [visible, setVisible] = React.useState(true);
   const pRef = React.useRef<HTMLParagraphElement>(null);
 
   React.useEffect(() => {
+    if (!visible) {
+      return;
+    }
+
     const rootElement = document.createElement('div');
     pRef.current?.parentElement?.appendChild(rootElement);
 
@@ -18,15 +24,31 @@ export default function App() {
 
     root.render(
       <React.StrictMode>
-        <StyleProvider container={shadowRoot}>
+        <StyleProvider container={shadowRoot} cache={createCache()}>
           <div style={{ border: '6px solid #000', padding: 8 }}>
             <h1>Shadow Root!</h1>
             <Button type="primary">Hello World!</Button>
+            <Spin />
           </div>
         </StyleProvider>
       </React.StrictMode>,
     );
-  }, []);
 
-  return <p ref={pRef} />;
+    return () => {
+      rootElement.remove();
+    };
+  }, [visible]);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        Trigger {String(visible)}
+      </button>
+      <p ref={pRef} />
+    </>
+  );
 }
