fa99a21e609177677f26cdf4978d4d5acb7e1acc
kiner-tang(文辉) <1127031143@qq.com>
2023-04-07 14:30:21 +0800

    feat: support extra plain style

--- src/hooks/useStyleRegister.tsx
+++ src/hooks/useStyleRegister.tsx
@@ -452,7 +452,7 @@ export default function useStyleRegister(
 // ============================================================================
 // ==                                  SSR                                   ==
 // ============================================================================
-export function extractStyle(cache: Cache) {
+export function extractStyle(cache: Cache, plain = false) {
   // prefix with `style` is used for `useStyleRegister` to cache style context
   const styleKeys = Array.from(cache.cache.keys()).filter((key) =>
     key.startsWith('style%'),
@@ -466,7 +466,9 @@ export function extractStyle(cache: Cache) {
     const [styleStr, tokenKey, styleId]: [string, string, string] =
       cache.cache.get(key)![1];
 
-    styleText += `<style ${ATTR_TOKEN}="${tokenKey}" ${ATTR_MARK}="${styleId}">${styleStr}</style>`;
+    styleText += plain
+      ? styleStr
+      : `<style ${ATTR_TOKEN}="${tokenKey}" ${ATTR_MARK}="${styleId}">${styleStr}</style>`;
   });
 
   return styleText;
