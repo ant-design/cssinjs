14a217942869a7961cac0866578c34d37f83ae56
LING_ZI_QING <onlylingstudio@gmail.com>
2023-04-21 10:26:15 +0800

    fix: doc publicPath

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,12 +1,16 @@
 // more config: https://d.umijs.org/config
 import { defineConfig } from 'dumi';
 
+const repo = process.env.PUBLIC_PATH || '';
+
 export default defineConfig({
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
   themeConfig: {
     name: 'cssinjs',
     logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
+  base: `/${repo}`,
+  publicPath: `/${repo}`,
   outputPath: '.doc',
   exportStatic: {},
 });
