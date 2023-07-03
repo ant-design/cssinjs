34c1d7b2da68b1f00f24547de17908c9eb4c0ecc
二货爱吃白萝卜 <smith3816@gmail.com>
2023-07-03 17:23:19 +0800

    fix: Suspense will make cache counter not sync (#120)
    * fix: cache counter
    
    * test: add StrictMode test
    
    * fix: StrictMode broken
    
    * chore: useEffect
    
    * refactor: merg

--- src/hooks/useGlobalCache.tsx
+++ src/hooks/useGlobalCache.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import StyleContext from '../StyleContext';
 import type { KeyType } from '../Cache';
+import StyleContext from '../StyleContext';
 import useHMR from './useHMR';
 
 export default function useClientCache<CacheType>(
@@ -14,32 +14,44 @@ export default function useClientCache<CacheType>(
 
   const HMRUpdate = useHMR();
 
+  type UpdaterArgs = [times: number, cache: CacheType];
+
+  const buildCache = (updater?: (data: UpdaterArgs) => UpdaterArgs) => {
+    globalCache.update(fullPath, (prevCache) => {
+      const [times = 0, cache] = prevCache || [];
+
+      // HMR should always ignore cache since developer may change it
+      let tmpCache = cache;
+      if (process.env.NODE_ENV !== 'production' && cache && HMRUpdate) {
+        onCacheRemove?.(tmpCache, HMRUpdate);
+        tmpCache = null;
+      }
+
+      const mergedCache = tmpCache || cacheFn();
+
+      const data: UpdaterArgs = [times, mergedCache];
+
+      // Call updater if need additional logic
+      return updater ? updater(data) : data;
+    });
+  };
+
   // Create cache
   React.useMemo(
-    () => {
-      globalCache.update(fullPath, (prevCache) => {
-        const [times = 0, cache] = prevCache || [];
-
-        // HMR should always ignore cache since developer may change it
-        let tmpCache = cache;
-        if (process.env.NODE_ENV !== 'production' && cache && HMRUpdate) {
-          onCacheRemove?.(tmpCache, HMRUpdate);
-          tmpCache = null;
-        }
-
-        const mergedCache = tmpCache || cacheFn();
-
-        return [times + 1, mergedCache];
-      });
-    },
+    () => buildCache(),
     /* eslint-disable react-hooks/exhaustive-deps */
     [fullPath.join('_')],
     /* eslint-enable */
   );
 
   // Remove if no need anymore
-  React.useEffect(
-    () => () => {
+  React.useEffect(() => {
+    // It's bad to call build again in effect.
+    // But we have to do this since StrictMode will call effect twice
+    // which will clear cache on the first time.
+    buildCache(([times, cache]) => [times + 1, cache]);
+
+    return () => {
       globalCache.update(fullPath, (prevCache) => {
         const [times = 0, cache] = prevCache || [];
         const nextCount = times - 1;
@@ -51,9 +63,8 @@ export default function useClientCache<CacheType>(
 
         return [times - 1, cache];
       });
-    },
-    fullPath,
-  );
+    };
+  }, fullPath);
 
   return globalCache.get(fullPath)![1];
 }
