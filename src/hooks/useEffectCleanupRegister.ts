8bfef177a2d2851138018f7e1337c43e76a6304b
MadCcc <1075746765@qq.com>
2023-08-09 21:34:18 +0800

    fix: should cleanup styles in useEffect cleanup function

---
+++ src/hooks/useEffectCleanupRegister.ts
@@ -0,0 +1,48 @@
+import { warning } from 'rc-util/lib/warning';
+import * as React from 'react';
+
+const fullClone = {
+  ...React,
+};
+const { useInsertionEffect } = fullClone;
+
+// DO NOT register functions in useEffect cleanup function, or functions that registered will never be called.
+const useCleanupRegister = () => {
+  const effectCleanups: (() => void)[] = [];
+  let cleanupFlag = false;
+  function register(fn: () => void) {
+    if (cleanupFlag) {
+      if (process.env.NODE_ENV !== 'production') {
+        warning(
+          false,
+          '[Ant Design CSS-in-JS] You are registering a cleanup function after unmount, which will not have any effect.',
+        );
+      }
+      return;
+    }
+    effectCleanups.push(fn);
+  }
+
+  React.useEffect(() => () => {
+    cleanupFlag = true;
+    if (effectCleanups.length) {
+      for (const fn of effectCleanups) {
+        fn();
+      }
+    }
+  });
+
+  return register;
+};
+
+const useRun = () => {
+  return function (fn: () => void) {
+    fn();
+  };
+};
+
+// Only enable register in React 18
+const useEffectCleanupRegister =
+  typeof useInsertionEffect !== 'undefined' ? useCleanupRegister : useRun;
+
+export default useEffectCleanupRegister;
